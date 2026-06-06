83f351379908f608c14f385ffb61852cfd078727
jitendrsuman-pixel <armygamingtotal@gmail.com>
2026-06-06 14:28:23 +0530

    Update GenerativeModel to use 'gemini-1.0-pro'

--- App.py
+++ App.py
@@ -55,7 +55,7 @@ else:
                 with st.spinner("Gemini AI आपके प्रोडक्ट के लिए बेस्ट 'Target Audience' और 'Ad Copy' बना रहा है..."):
                     try:
                         genai.configure(api_key=gemini_key)
-                        model = genai.GenerativeModel('gemini-1.5-flash') 
+                        model = genai.GenerativeModel('gemini-1.0-pro')
 
                         prompt = f"""
                         You are an expert Facebook Ads Marketer. For the product described below, give:
